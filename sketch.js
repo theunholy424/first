d26046b05a16752fe3dbbdaa33191b958b087e43
theunholy424 <82865973+theunholy424@users.noreply.github.com>
2022-03-17 22:25:29 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,6 +17,8 @@ let animals = [{
     ];
 
     let randomIndex;
+    let counter = 0;
+    let animating = false;
 
     function setup() {
       createCanvas(600, 600);
@@ -24,21 +26,41 @@ let animals = [{
       textSize(32);
       text("click to randomize",50,50);
 
-
-
+      setTimeout(changeBackground, 1000);
     }
     function draw(){
-
-
+      if(animating == true){
+        ellipse(random(width), random(height), random(50, 200))
+      }
     }
-    function mousePressed(){
+
+    function randomizer(){
+      animating = false;
       if (animals[0]) {
         background(random(200,255));
-      randomIndex = int(random(animals. length));
-      text(animals[randomIndex].name, 50, 50);
+     randomIndex = int(random(animals. length));
+      //text(animals[randomIndex].name + "’s favorite color is" +
+      //animals[randomIndex].color, 50, 50);
+      text(`${animals[randomIndex].name} ’s favorite color is
+      ${animals[randomIndex].color}`, 50, 50);
       animals. splice(randomIndex, 1);
-    } else {
-      background(random(200,255));
-      text("nothing left",50,50);
+  } else {
+     background(random(200,255));
+     text("nothing left",50,50);
+ }
+    }
+    function changeBackground(){
+      if (counter <= 5){
+       counter++;
+        console.log(counter)
+        background(random(255), random(255), random(255));
+      setTimeout(changeBackground, 1000);
+    } else{
+
     }
     }
+    function mousePressed(){
+      animating = true;
+      setTimeout(randomizer, 2000);
+
+   }
